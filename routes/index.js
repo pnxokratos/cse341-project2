899113f8bdbb0d392bf655ade530115b8f91719a
pnxokratos <her19054@byui.edu>
2023-03-16 00:04:42 +0100

    AOAUTH

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 router.use('/', require('./swagger'));
 router.use('/todos', require('./todos'))
 
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-const dotenv = require('dotenv');
-
+const dotenv = require("dotenv");
+dotenv.config();
+const { auth, requiresAuth } = require('express-openid-connect');
 
 const port = process.env.PORT || 8080;
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.secret,
+  baseURL: process.env.baseURL,
+  clientID: process.env.clientID,
+  issuerBaseURL: process.env.issuerBaseURL
+};
 
 const app = express();
 
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 app
   .use(bodyParser.json())
@@ -24,4 +35,12 @@ mongodb.initDb((err) => {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
   }
+});
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });
