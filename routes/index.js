9742919bdd36884a1a08175afa3bc53b216f2081
pnxokratos <her19054@byui.edu>
2023-03-19 00:01:48 +0100

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 router.use('/', require('./swagger'));
-router.use('/todos', require('./todos'))
+router.use('/todos', require('./todos'));
+router.use('/tasks', require('./tasks'))
 
 module.exports = router;
