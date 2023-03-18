35d2e7ac4cf91accedf49783cbc1a3cac627b20d
pnxokratos <her19054@byui.edu>
2023-03-18 23:34:10 +0100

    swagger

--- routes/swagger.js
+++ routes/swagger.js
@@ -4,7 +4,6 @@ const swaggerDocument = require('../swagger.json');
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-router.use('/', require('./todos'));
-router.use('/', require('./tasks'));
+
 
 module.exports = router;
